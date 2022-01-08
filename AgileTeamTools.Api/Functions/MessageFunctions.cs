65c0a12022fc0d167e4c5da00d33de27bf6e1841
Josh Rolstad <jrolstad@gmail.com>
2022-01-08 07:07:12 -0800

    Adding SignalR function

---
+++ AgileTeamTools.Api/Functions/MessageFunctions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace AgileTeamTools.Api.Functions
+{
+    public static class MessageFunctions
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "messages")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+    }
+}
